baa21fde3ff279568e0356e4d98e81c8cbc2998d
IgorMuramshchikov <igor.muram@gmail.com>
2021-09-01 23:52:24 +0700

    Update WebServer

--- Lab4/Server/main.cpp
+++ Lab4/Server/main.cpp
@@ -1,26 +1,16 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
-const int BuffSize = 256;
+using namespace std;
+const int BuffSize = 8192;
 
-sockaddr CreateAddress(std::string ip, uint16_t port)
-{
-	sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
-	inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
-
-	return *reinterpret_cast<sockaddr*>(&addr);
-}
-
-void HandleError(std::string message)
-{
-	std::cout << "ERROR. " << message << std::endl;
-	system("pause");
-	exit(1);
-}
+addrinfo CreateAddress(string ip, string port);
+void HandleError(string message);
+void HandleGetRequest(string data, SOCKET socket);
 
 int main()
 {
@@ -28,32 +18,119 @@ int main()
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		HandleError("Failed to initialize library.");
 
-	SOCKET listenSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	addrinfo addr = CreateAddress("0.0.0.0", "2005");
+
+	SOCKET listenSock = socket(addr.ai_family, addr.ai_socktype, addr.ai_protocol);
 	if (listenSock == INVALID_SOCKET)
 		HandleError("Failed to create socket.");
 
-	sockaddr addr = CreateAddress("127.0.0.1", 4790);
-
-	if (bind(listenSock, &addr, sizeof(sockaddr)) != 0)
+	if (bind(listenSock, addr.ai_addr, addr.ai_addrlen) != 0)
 		HandleError("Failed to bind socket.");
 
 	if (listen(listenSock, SOMAXCONN) != 0)
 		HandleError("Failed to listen.");
 
-	char data[BuffSize];
 	while (true)
 	{
 		SOCKET clientSock = accept(listenSock, NULL, NULL);
 
-		if (recv(clientSock, data, BuffSize, NULL) == SOCKET_ERROR)
+		char data[BuffSize];
+		int result = recv(clientSock, data, BuffSize, NULL);
+		if (result == SOCKET_ERROR)
 			HandleError("Failed to receive data from client.");
 
-		std::cout << "Data from client: " << data << std::endl;
+		data[result] = '\0';
+		std::cout << "Client request:\n" << data << std::endl;
+
+		string tempData(data);
+
+		if (tempData.find("GET", 0) == 0)
+			HandleGetRequest(tempData, clientSock);
 
 		closesocket(clientSock);
 	}
 
+	freeaddrinfo(&addr);
 	WSACleanup();
 	system("pause");
 	return 0;
+}
+
+addrinfo CreateAddress(string ip, string port)
+{
+	addrinfo* addr = nullptr;
+	addrinfo hints = { };
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	getaddrinfo(ip.c_str(), port.c_str(), &hints, &addr);
+
+	return *addr;
+}
+
+void HandleError(string message)
+{
+	cout << "ERROR. " << message << endl;
+	system("pause");
+	exit(1);
+}
+
+void HandleGetRequest(string data, SOCKET socket)
+{
+	string filename = data.substr(5, data.find_first_of(' ', 5) - 5);
+
+	stringstream response;
+	string responseBody;
+
+	response
+		<< "HTTP/1.1 200 OK\r\n"
+		<< "Server: ninja\r\n"
+		<< "Content-Type: text/html\r\n";
+
+	if (filename == "")
+	{
+		ifstream in("index.html");
+
+		in.seekg(0, in.end);
+		int filelength = in.tellg();
+		in.seekg(0, in.beg);
+
+		response
+			<< "Content-Length: " << filelength
+			<< "\r\n\r\n";
+
+		responseBody.resize(filelength, ' ');
+		in.read(&responseBody[0], filelength);
+	}
+	else
+	{
+		ifstream in(filename);
+
+		in.seekg(0, in.end);
+		int filelength = in.tellg();
+		in.seekg(0, in.beg);
+
+		if (!in.fail())
+		{
+			cout << "File " << filename << " found" << endl << endl;
+
+			response
+				<< "Content-Length: " << filelength
+				<< "\r\n\r\n";
+
+			responseBody.resize(filelength, ' ');
+			in.read(&responseBody[0], filelength);
+		}
+		else
+		{
+			cout << "File " << filename << " not found" << endl << endl;
+			response << "HTTP/1.1 404 File not found\r\n\r\n";
+			responseBody = "<h1 style=\"text-align: center;\">404 - File not found</h1>";
+		}
+	}
+
+	send(socket, response.str().c_str(), response.str().length(), NULL);
+	send(socket, responseBody.c_str(), responseBody.length(), NULL);
 }
